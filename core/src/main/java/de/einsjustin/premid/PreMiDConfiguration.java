06048b5338ac2831e3b0b55f966a35ae914fbe86
Justin <me@jxtn.de>
2025-11-15 11:14:25 +0100

    add activity template

--- core/src/main/java/de/einsjustin/premid/PreMiDConfiguration.java
+++ core/src/main/java/de/einsjustin/premid/PreMiDConfiguration.java
@@ -1,9 +1,14 @@
 package de.einsjustin.premid;
 
+import de.einsjustin.premid.ui.activity.PreMiDHowToUseActivity;
+import net.labymod.api.Laby;
 import net.labymod.api.addon.AddonConfig;
+import net.labymod.api.client.gui.screen.widget.widgets.input.ButtonWidget.ButtonSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
+import net.labymod.api.configuration.settings.Setting;
+import net.labymod.api.util.MethodOrder;
 
 @ConfigName("settings")
 public class PreMiDConfiguration extends AddonConfig {
@@ -15,6 +20,12 @@ public class PreMiDConfiguration extends AddonConfig {
 
   @SwitchSetting
   private final ConfigProperty<Boolean> shareActivity = new ConfigProperty<>(true);
+
+  @MethodOrder(after = "shareActivity")
+  @ButtonSetting
+  public void howToUse(Setting setting) {
+    Laby.labyAPI().minecraft().minecraftWindow().displayScreen(new PreMiDHowToUseActivity());
+  }
 
   @Override
   public ConfigProperty<Boolean> enabled() {
---
+++ core/src/main/java/de/einsjustin/premid/ui/activity/PreMiDHowToUseActivity.java
@@ -0,0 +1,16 @@
+package de.einsjustin.premid.ui.activity;
+
+import net.labymod.api.client.gui.screen.Parent;
+import net.labymod.api.client.gui.screen.activity.AutoActivity;
+import net.labymod.api.client.gui.screen.activity.Link;
+import net.labymod.api.client.gui.screen.activity.types.SimpleActivity;
+
+@Link("how-to-use.lss")
+@AutoActivity
+public class PreMiDHowToUseActivity extends SimpleActivity {
+
+  @Override
+  public void initialize(Parent parent) {
+    super.initialize(parent);
+  }
+}
